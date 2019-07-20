65485242faf2f2722b80c3e32cbb4d6ba523baa1
adnanalimurtaza <adnan.ali.murtaza@gmail.com>
2019-07-21 02:04:53 +0300

    add new resources or endpoints.

--- src/index.js
+++ src/index.js
@@ -11,9 +11,41 @@ const port = process.env.PORT || 8000;
 app.use( cors() );
 
 // define the resolve handler for the default home page
-app.get( "/", async ( req, res ) => {
-    return res.send( JSON.stringify( "Welcome to Pked." ) );
-} );
+// try different HTTP methods with same URI '/' which acts as resource
+app.get( "/", ( req, res ) => {
+    return res.send( `Recieved a GET HTTP method request.` );
+});
+
+app.post( "/", ( req, res )  => {
+    return res.send( `Recieved a POST HTTP method request.` );
+});
+
+app.put( "/", (req, res) => {
+    return res.send( `Recieved a PUT HTTP method request.` );
+});
+
+app.delete( "/", (req, res) => {
+    return res.send( `Recieved a DELETE HTTP method request.` );
+});
+
+// add '/user' resouce or endpoint 
+app.get( "/users", ( req, res ) => {
+    return res.send( `Recieved a GET users request.` );
+});
+
+app.post( "/users", ( req, res )  => {
+    return res.send( `Recieved a POST users request.` );
+});
+
+// assign parameters in URI, callback func holds it in request object
+app.put( "/users/:userId", (req, res) => {
+    return res.send( `Recieved a PUT request for userId:${req.params.userId}.` );
+});
+
+// assign parameters in URI, callback func holds it in request object
+app.delete( "/users/:userId", (req, res) => {
+    return res.send( `Recieved a DELETE request for userId:${req.params.userId}.` );
+});
 
 // start the express server
 app.listen( port, () => {
