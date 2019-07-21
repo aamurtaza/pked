ac9cdbff7f79b80902deec8d6f661c07fdf9c532
adnanalimurtaza <adnan.ali.murtaza@gmail.com>
2019-07-21 18:25:41 +0300

    add note 'not in use' in start.js

--- start.js
+++ start.js
@@ -1,3 +1,5 @@
+// NOTE: This file is in use. Babel 7 doesn't requires start.js solution
+
 // Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
 require("@babel/register")({
     presets: ["@babel/preset-env"]
